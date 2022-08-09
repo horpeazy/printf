ae21b6ea11b3cfdbfa93ff6d12e9b801f95f952c
horpeazy <iyamuhope.nosa647@gmail.com>
2022-08-08 17:12:05 -0700

    feta: added u,o,x,X conversion specifier fucntionality

--- get_print.c
+++ get_print.c
@@ -18,6 +18,10 @@ int (*get_print(char s))(va_list, flags_t *)
 		{'i', print_int},
 		{'d', print_int},
 		{'b', print_binary},
+		{'u', print_unsigned},
+		{'o', print_octal},
+		{'x', print_hex},
+		{'X', print_hex_big},
 		};
 	int flags = 5;
 
